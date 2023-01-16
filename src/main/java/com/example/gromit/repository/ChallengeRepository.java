7c47069fe6526bb6447f697b38aa94e160a1235d
HwaSup <ghktjq1119@naver.com>
2023-01-16 18:10:57 +0900

    feat : 디펜던시 추가 및 Security Config 임시 설정

---
+++ src/main/java/com/example/gromit/repository/ChallengeRepository.java
@@ -0,0 +1,9 @@
+package com.example.gromit.repository;
+
+import com.example.gromit.entity.Challenge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ChallengeRepository extends JpaRepository<Challenge,Long> {
+}
---
+++ src/main/java/com/example/gromit/repository/UserAccountRepository.java
@@ -0,0 +1,11 @@
+package com.example.gromit.repository;
+
+import com.example.gromit.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserAccountRepository extends JpaRepository<UserAccount,Long> {
+
+
+}
